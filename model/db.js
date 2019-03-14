ae649e7eb956c684e2cb6e2287f017ebf52894dd
Shreya <shreyabhujbal27@gmail.com>
2019-03-14 20:41:20 +0530

    db done

--- model/db.js
+++ model/db.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose"),
   database =
-    "mongodb://shreyabhujbal:shreyabhujbal27@ds213896.mlab.com:13896/cres";
+    "mongodb://shreyabhujbal:shreyabhujbal*27@ds213896.mlab.com:13896/cres";
 mongoose.connect(database, { useNewUrlParser: true });
 mongoose.connection.on("connected", () => {
   console.log("connected");
