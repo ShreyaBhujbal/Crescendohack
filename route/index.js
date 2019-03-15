edd019a43f0e8f98f5115e8924395b841fc9eee6
Shreya <shreyabhujbal27@gmail.com>
2019-03-15 15:13:36 +0530

    node mailer

--- route/index.js
+++ route/index.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var router = express.Router();
 var mongoose = require("mongoose");
+//mail
+var nodemailer = require("nodemailer");
+var app = express();
 
- var eventCtrl = require("../controller/event");
- var ngoCtrl = require("../controller/ngo");
+var eventCtrl = require("../controller/event");
+var ngoCtrl = require("../controller/ngo");
 //
 // router
 //   .route("/event")
@@ -15,37 +18,70 @@ var mongoose = require("mongoose");
 //   .get(ngoCtrl.getngo)
 //   .post(ngoCtrl.addNgo);
 
-  /* GET home page. */
-  router.get('/', function(req, res, next) {
-    res.render('index', {page:'Home', menuId:'home'});
-  });
+/* GET home page. */
+router.get("/", function(req, res, next) {
+  res.render("index", { page: "Home", menuId: "home" });
+});
 
-  router.get('/about', function(req, res, next) {
-    res.render('about', {page:'About Us', menuId:'about'});
-  });
-  router.get('/NGO', function(req, res, next) {
-    res.render('NGO', {page:'Add Event', menuId:'NGO'});
-  });
+router.get("/about", function(req, res, next) {
+  res.render("about", { page: "About Us", menuId: "about" });
+});
+router.get("/NGO", function(req, res, next) {
+  res.render("NGO", { page: "Add Event", menuId: "NGO" });
+});
 
-  router
-  .route('/login')
+router
+  .route("/login")
   .get(function(req, res, next) {
-    res.render('login', {page:'LOGIN', menuId:'login'});
+    res.render("login", { page: "LOGIN", menuId: "login" });
   })
-  .post(ngoCtrl.viewNgo)
+  .post(ngoCtrl.viewNgo);
 
-  router
-  .route('/register')
+router
+  .route("/register")
   .get(function(req, res, next) {
-    res.render('register', {page:'REGISTER', menuId:'register'});
+    res.render("register", { page: "REGISTER", menuId: "register" });
   })
-  .post(ngoCtrl.addNgo)
+  .post(ngoCtrl.addNgo);
 
-  router
-  .route('/searchNgo')
-  .get(function(req, res, next) {
-    res.render('searchNgo', {page:'SEARCH NGO', menuId:'searchNgo'});
-  })
-  
+router.route("/searchNgo").get(function(req, res, next) {
+  res.render("searchNgo", { page: "SEARCH NGO", menuId: "searchNgo" });
+});
+
+//mail
+
+var smtpTransport = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  auth: {
+    user: "sanesassesor@gmail.com",
+    pass: "sanes4ever"
+  }
+});
+
+router.route("/ngo/mail").get(function(req, res, next) {
+  res.render("mail");
+});
+
+router.route("/ngo/mail/send").get(function(req, res) {
+  //code to send e-mail.
+  //Will be shown soon.
+  var mailOptions = {
+    to: req.query.to,
+    subject: req.query.subject,
+    text: req.query.text
+  };
+  console.log(req);
+  console.log(mailOptions);
+  smtpTransport.sendMail(mailOptions, function(error, response) {
+    if (error) {
+      console.log(error);
+      res.send("error");
+    } else {
+      console.log("Message sent: " + response.message);
+      res.send("sent");
+    }
+  });
+});
 
 module.exports = router;
